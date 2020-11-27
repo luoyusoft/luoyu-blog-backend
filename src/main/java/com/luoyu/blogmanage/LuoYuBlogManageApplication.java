542aac15907543ae22e097559925c50714382e89
luoyusoft <luoyusoft@126.com>
2020-11-27 10:37:49 +0800

    1. 新增nacos组件作为配置中心，注册中心 2. 修复日志打印问题

--- src/main/java/com/luoyu/blogmanage/LuoYuBlogManageApplication.java
+++ src/main/java/com/luoyu/blogmanage/LuoYuBlogManageApplication.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
+@EnableDiscoveryClient
 @MapperScan({"com.luoyu.blogmanage.mapper"})
 @SpringBootApplication
 public class LuoYuBlogManageApplication {
