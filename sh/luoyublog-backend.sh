7ff6c4b109de803c4851ffaab74622447d492aa8
luoyusoft <luoyusoft@126.com>
2020-12-06 16:28:22 +0800

    1. 修改脚本

--- sh/luoyublog-backend.sh
+++ sh/luoyublog-backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl https://luoyublog.com/api/luoyublog/actuator/shutdown -X POST
 sleep 20
-docker rm luoyublog-backend
-docker rmi luoyusoft/luoyublog-backend:latest
-docker run -dit --name luoyublog-backend -v /usr/local/docker/luoyublog-backend/log:/usr/local/project/luoyublog-backend/log -p 8800:8800 -p 9999:9999 luoyusoft/luoyublog-backend:latest
+docker rm luoyublog
+docker rmi luoyusoft/luoyublog:latest
+docker run -dit --name luoyublog -v /usr/local/docker/luoyublog-backend/log:/usr/local/project/luoyublog-backend/log -p 8800:8800 -p 9999:9999 luoyusoft/luoyublog:latest
